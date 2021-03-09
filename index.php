ad76784b18b66deb2ad5a69b6cad79ed7685e9ae
ohno331 <e.ohno331@gmail.com>
2021-03-09 12:10:15 +0900

    session

--- index.php
+++ index.php
@@ -1,98 +1,33 @@
+
+<!-- セッション -->
+<div class="log">
 <?php
- 
-
-$year = date('Y');
-$month = date('n');
- 
-
-$last_day = date('j', mktime(0, 0, 0, $month + 1, 0, $year));
- 
-$calendar = array();
-$j = 0;
- 
-
-for ($i = 1; $i < $last_day + 1; $i++) {
- 
-    
-    $week = date('w', mktime(0, 0, 0, $month, $i, $year));
- 
-    
-    if ($i == 1) {
- 
-        // 1日目の曜日までをループ
-        for ($s = 1; $s <= $week; $s++) {
- 
-            // 前半に空文字をセット
-            $calendar[$j]['day'] = '';
-            $j++;
- 
-        }
- 
-    }
- 
-    
-    $calendar[$j]['day'] = $i;
-    $j++;
- 
-    
-    if ($i == $last_day) {
- 
-        
-        for ($e = 1; $e <= 6 - $week; $e++) {
- 
-            
-            $calendar[$j]['day'] = '';
-            $j++;
- 
-        }
- 
-    }
- 
+session_start();
+session_regenerate_id(true);
+if(isset($_SESSION['member_login'])==false)
+{
+	print 'ようこそゲスト様　';
+	print '<a href="member_login.html">会員ログイン</a><br />';
+	print '<br />';
 }
- 
+else
+{
+	print 'ようこそ　';
+	print $_SESSION['member_name'];
+	print ' 様　';
+	print '<a href="member_logout.php">ログアウト</a><br />';
+	print '<br />';
+}
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-
-<br>
-<br>
-<table>
-<caption><?php echo $year; ?>年<?php echo $month; ?>月のカレンダー</caption>
-
-    <tr>
-        <th>日</th>
-        <th>月</th>
-        <th>火</th>
-        <th>水</th>
-        <th>木</th>
-        <th>金</th>
-        <th>土</th>
-    </tr>
- 
-    <tr>
-    <?php $cnt = 0; ?>
-    <?php foreach ($calendar as $key => $value): ?>
- 
-        <td>
-        <?php $cnt++; ?>
-        <?php echo $value['day']; ?>
-        </td>
- 
-    <?php if ($cnt == 7): ?>
-    </tr>
-    <tr>
-    <?php $cnt = 0; ?>
-    <?php endif; ?>
- 
-    <?php endforeach; ?>
-    </tr>
-</table>
+    
 </body>
 </html>
